a542199b4ae618cf4700daafaf8c659e69a0426f
hzrd149 <github@hzrd149.com>
2026-08-04 15:17:43 +0100

    Remove comic-upload napplet

--- napplets/comic-upload/src/lib/hash.ts
+++
@@ -1,5 +0,0 @@
-export async function sha256Hex(data: Blob | ArrayBuffer): Promise<string> {
-  const arrayBuffer = data instanceof Blob ? await data.arrayBuffer() : data;
-  const digest = await crypto.subtle.digest('SHA-256', arrayBuffer);
-  return Array.from(new Uint8Array(digest), (byte) => byte.toString(16).padStart(2, '0')).join('');
-}
